8c7c65efd636ec3de19bc6882569a538275fa6a0
Chanha Park <qkrcksgk02@korea.ac.kr>
2022-05-25 04:35:11 +0900

    GNL

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: chanhapa <chanhapa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 14:27:09 by chanhapa          #+#    #+#             */
-/*   Updated: 2022/05/12 20:23:13 by chanhapa         ###   ########.fr       */
+/*   Updated: 2022/05/19 18:24:10 by chanhapa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 char	*get_next_line(int fd)
 {
-	static char	*backup[OPEN_MAX];
-	char		buff[BUFFER_SIZE + 1];
+	static char	*backup;
+	char		*buff;
 	char		*line;
 	int			read_line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
+	buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	while ((read_line = read(fd, buff, BUFFER_SIZE)) > 0)
 	{
 		backup[fd] = ft_strjoin(backup[fd], buff);
